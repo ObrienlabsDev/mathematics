51aa9d418a3eb611d90ca3b5bc1384c6c55f7f5c
Michael O'Brien <michael@obrienlabs.org>
2025-07-03 22:51:22 -0400

    #1 - start matrix datastructure

--- src/main/java/dev/obrienlabs/mathematics/MatrixImpl.java
+++ src/main/java/dev/obrienlabs/mathematics/MatrixImpl.java
@@ -1,7 +1,15 @@
 package dev.obrienlabs.mathematics;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class MatrixImpl implements Matrix {
 
+	
+	private Map<Integer, Number> rows = new ConcurrentHashMap<>();
+	private Map<Integer, Number> columns = new ConcurrentHashMap<>();
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
@@ -23,6 +31,22 @@ public class MatrixImpl implements Matrix {
 	public Matrix multiply(Matrix m1) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public Map<Integer, Number> getRows() {
+		return rows;
+	}
+
+	public void setRows(Map<Integer, Number> rows) {
+		this.rows = rows;
+	}
+
+	public Map<Integer, Number> getColumns() {
+		return columns;
+	}
+
+	public void setColumns(Map<Integer, Number> columns) {
+		this.columns = columns;
 	}
 
 }
