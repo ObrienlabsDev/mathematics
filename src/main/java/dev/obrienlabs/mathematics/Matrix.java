a080f76f67515f37cbcdcff6bb6db0a63f518683
Michael O'Brien <michael@obrienlabs.org>
2025-07-05 21:24:01 -0400

    #1 - initial int array streaming

--- src/main/java/dev/obrienlabs/mathematics/Matrix.java
+++ src/main/java/dev/obrienlabs/mathematics/Matrix.java
@@ -2,8 +2,14 @@ package dev.obrienlabs.mathematics;
 
 public interface Matrix {
 
+	void set(int r, int c, int value);
+	int get(int r, int c);
 	Matrix add(Matrix m1);
 	Matrix subtract(Matrix m1);
 	Matrix multiply(Matrix m1);
+	
+	// stats
+	int rowSize();
+	int colSize();
 	
 }
--- src/main/java/dev/obrienlabs/mathematics/MatrixImpl.java
+++ src/main/java/dev/obrienlabs/mathematics/MatrixImpl.java
@@ -1,24 +1,48 @@
 package dev.obrienlabs.mathematics;
 
+import java.util.Arrays;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.IntStream;
 
 public class MatrixImpl implements Matrix {
 
+	// Primitives
+	private int rowSize = 4;
+	private int colSize = 4;
+	int[][] cells;// = new int[rowSize][colSize];
 	
-	private Map<Integer, Number> rows = new ConcurrentHashMap<>();
-	private Map<Integer, Number> columns = new ConcurrentHashMap<>();
+	
+	// OO
+	private Map<Integer, Number> rowMap = new ConcurrentHashMap<>();
+	private Map<Integer, Number> colMap = new ConcurrentHashMap<>();
 	
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public MatrixImpl(int rows, int cols) {
+		rowSize = rows;
+		colSize = cols;
+		cells = new int[rowSize][colSize];
 	}
-
+	
+	@Override
+	public int get(int r, int c) {
+		return cells[r][c];
+	}
+	
+	@Override
+	public void set(int r, int c, int value) {
+		cells[r][c] = value;
+	}
+	
 	@Override
 	public Matrix add(Matrix m1) {
-		// TODO Auto-generated method stub
-		return null;
+		Matrix m3 = new MatrixImpl(m1.rowSize(), m1.colSize());
+		for(int r=0;r<m3.rowSize(); r++) {
+			for(int c=0;c<m3.colSize(); c++) {
+				m3.set(r, c, m1.get(r, c) + this.get(r, c));
+			}
+		}
+		return m3;
 	}
 
 	@Override
@@ -33,20 +57,40 @@ public class MatrixImpl implements Matrix {
 		return null;
 	}
 
-	public Map<Integer, Number> getRows() {
-		return rows;
+	public Map<Integer, Number> getRowMap() { return rowMap; }
+
+	public void setRowMap(Map<Integer, Number> rows) { this.rowMap = rows; }
+
+	public Map<Integer, Number> getColMap() { return colMap; }
+
+	public void setColMap(Map<Integer, Number> columns) { this.colMap = columns; }
+
+	@Override
+	public int rowSize() { return rowSize; }
+
+	@Override
+	public int colSize() { return colSize; }
+	
+	
+	@Override
+	public String toString() {
+		StringBuffer buffer = new StringBuffer("[");
+		IntStream flat = Arrays.stream(cells).flatMapToInt(Arrays::stream);
+		flat.forEach(c -> buffer.append(c));
+		buffer.append("]");
+		return buffer.toString();
+	}
+	
+
+	public static void main(String[] args) {
+		Matrix m1 = new MatrixImpl(4,4);
+		Matrix m2 = new MatrixImpl(4,4);
+		m1.set(0, 0, 1);
+		m2.set(0, 0, 2);
+		Matrix m3 = m1.add(m2);
+		System.out.println(m3);
+		
 	}
 
-	public void setRows(Map<Integer, Number> rows) {
-		this.rows = rows;
-	}
-
-	public Map<Integer, Number> getColumns() {
-		return columns;
-	}
-
-	public void setColumns(Map<Integer, Number> columns) {
-		this.columns = columns;
-	}
-
+	
 }
