9b9388cd8b0572d0236044e3b7fd414f03b98828
Michael O'Brien <michael@obrienlabs.org>
2025-07-06 20:41:37 -0400

    #1 - use single dim vector

--- src/main/java/dev/obrienlabs/mathematics/Matrix.java
+++ src/main/java/dev/obrienlabs/mathematics/Matrix.java
@@ -3,7 +3,9 @@ package dev.obrienlabs.mathematics;
 public interface Matrix {
 
 	void set(int r, int c, double value);
+	void set(int pos, double value);
 	double get(int r, int c);
+	double get(int pos);
 	Matrix add(Matrix m1);
 	Matrix subtract(Matrix m1);
 	Matrix multiply(Matrix m1);
--- src/main/java/dev/obrienlabs/mathematics/MatrixImpl.java
+++ src/main/java/dev/obrienlabs/mathematics/MatrixImpl.java
@@ -5,43 +5,40 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.IntStream;
 
+/**
+ * Option 1: brute force 2 dimensional array
+ * Option 2: single dimensional array - using positional r/c arithmetic
+ */
 public class MatrixImpl implements Matrix {
 
 	// Primitives
-	private int rowSize = 4;
-	private int colSize = 4;
-	double[][] cells;// = new double[rowSize][colSize];
+	private final int rowSize;
+	private final int colSize;
+	double[] cells;// = new double[rowSize][colSize];
 	
 	
 	// OO
-	private Map<Integer, Number> rowMap = new ConcurrentHashMap<>();
+	private Map<Integer, Map<Integer, Number>> rowMap = new ConcurrentHashMap<>();
 	private Map<Integer, Number> colMap = new ConcurrentHashMap<>();
 	
 	
 	public MatrixImpl(int rows, int cols) {
 		rowSize = rows;
 		colSize = cols;
-		cells = new double[rowSize][colSize];
-	}
-	
-	@Override
-	public double get(int r, int c) {
-		return cells[r][c];
-	}
-	
-	@Override
-	public void set(int r, int c, double value) {
-		cells[r][c] = value;
+		cells = new double[rowSize * colSize];
 	}
 	
 	@Override
 	public Matrix add(Matrix m1) {
 		Matrix m3 = new MatrixImpl(m1.rowSize(), m1.colSize());
-		for(int r=0;r<m3.rowSize(); r++) {
-			for(int c=0;c<m3.colSize(); c++) {
-				m3.set(r, c, m1.get(r, c) + this.get(r, c));
-			}
-		}
+		//for(int r=0;r<m3.rowSize(); r++) {
+		//	for(int c=0;c<m3.colSize(); c++) {
+		//		m3.set(r, c, m1.get(r, c) + this.get(r, c));
+		//	}
+		//}
+		// create an iterative sequence and get both arrays by position
+		IntStream.range(0, rowSize * colSize)
+			.forEach(i -> m3.set(i, m1.get(i) + this.get(i)));
 		return m3;
 	}
 
@@ -57,9 +54,9 @@ public class MatrixImpl implements Matrix {
 		return null;
 	}
 
-	public Map<Integer, Number> getRowMap() { return rowMap; }
+	public Map<Integer, Map<Integer, Number>> getRowMap() { return rowMap; }
 
-	public void setRowMap(Map<Integer, Number> rows) { this.rowMap = rows; }
+	public void setRowMap(Map<Integer, Map<Integer, Number>> rows) { this.rowMap = rows; }
 
 	public Map<Integer, Number> getColMap() { return colMap; }
 
@@ -67,31 +64,40 @@ public class MatrixImpl implements Matrix {
 
 	@Override
 	public int rowSize() { return rowSize; }
-
 	@Override
 	public int colSize() { return colSize; }
-	
+	@Override
+	public double get(int r, int c) { return cells[r * colSize + c]; }
+	@Override
+	public void set(int r, int c, double value) { cells[r * colSize + c] = value; }
+	@Override
+	public void set(int pos, double value) { this.cells[pos] = value; }
+	@Override
+	public double get(int pos) { return this.cells[pos]; }
 	
 	@Override
 	public String toString() {
 		StringBuffer buffer = new StringBuffer("[");
 		//IntStream flat = Arrays.stream(cells).flatMapToInt(Arrays::stream);
 		//flat.forEach(c -> buffer.append(c));
-		Arrays.stream(cells).forEach(r -> {
-			buffer.append("[");
-			Arrays.stream(r).forEach(c -> buffer.append(c).append(","));
-			buffer.append("]");
-		});
+		//Arrays.stream(cells).forEach(r -> {
+		//	buffer.append("[");
+		//	Arrays.stream(r).forEach(c -> buffer.append(c).append(","));
+		//	buffer.append("]");
+		//});
+		
+		Arrays.stream(cells).forEach(c -> buffer.append(c).append(","));
 		buffer.append("]");
 		return buffer.toString();
 	}
 	
-
 	public static void main(String[] args) {
 		Matrix m1 = new MatrixImpl(4,4);
 		Matrix m2 = new MatrixImpl(4,4);
 		m1.set(0, 0, 1);
 		m2.set(0, 0, 2);
+		m1.set(2, 1, 1);
+		m2.set(1, 4, 2);
 		Matrix m3 = m1.add(m2);
 		System.out.println(m3);
 		
