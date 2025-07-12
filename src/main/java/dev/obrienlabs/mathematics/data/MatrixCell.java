ced0417532749c385f9ff8f605f6bbded1545050
Michael O'Brien <michael@obrienlabs.org>
2025-07-11 22:45:15 -0400

    #1 - start MatrixCell

---
+++ src/main/java/dev/obrienlabs/mathematics/data/MatrixCell.java
@@ -0,0 +1,17 @@
+package dev.obrienlabs.mathematics.data;
+
+import java.util.List;
+
+public class MatrixCell {
+
+	private List<MatrixCell> rowCells;
+	private List<MatrixCell> colCells;
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
